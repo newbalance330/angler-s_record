ab7aa911704809f69aaa32cecd7e2aaab6295684
newbalance330 <92163228+newbalance330@users.noreply.github.com>
2022-01-24 11:13:09 +0000

    [Fix]レイアウト微調整

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
+   # ログインなしで見れるページを指定
+   before_action :authenticate_user!,except: [:top, :about]
 
-   before_action :authenticate_user!,except: [:top]
    before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
